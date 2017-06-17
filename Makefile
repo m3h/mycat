ffdaab200c849ec82d6e80e66e29f77e36bdd89d
Reece Volker <reecevolker@gmail.com>
2017-06-17 21:55:03 +0200

    here git

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 mycat: cmdline.o mycat.o
-	gcc -o mycat cmdline.o mycat.o
+	cc -o mycat cmdline.o mycat.o
 
 cmdline.c: cmdline.ggo
 	gengetopt --input=cmdline.ggo --unamed-opts
 cmdline.h: cmdline.c
 
 cmdline.o: cmdline.c cmdline.h
-	gcc -c cmdline.c
+	cc -c cmdline.c
 mycat.o: mycat.c
-	gcc -c mycat.c
+	cc -c mycat.c
 
 clean:
 	rm -rf *.o cmdline.c cmdline.h
--- mycat.c
+++ mycat.c
@@ -7,6 +7,8 @@
 const char *stdinarg = "-";
 char *append;
 
+char *changeTab(char *lineptr);
+
 int
 main (int argc, char **argv)
 {
@@ -30,6 +32,12 @@ main (int argc, char **argv)
 	else
 		append = "";
 
+	/* for --numer */
+	unsigned long noLines = 0;
+
+	/* for squeeze-blank */
+	int needsqueeze;
+
 	int i;
 	for (i = 0; i < ai.inputs_num || nostdinarg; ++i) {
 
@@ -43,9 +51,32 @@ main (int argc, char **argv)
 
 		ssize_t read;
 		while (	(read = getline(&lineptr, &n, curFile)) != -1) {
+
+			if (ai.squeeze_blank_flag) {
+				if (strcmp("\n", lineptr) == 0) {
+					if (!needsqueeze)
+						needsqueeze = 1;
+					else
+						continue;
+				}
+			}
+			else {
+				needsqueeze = 0;
+			}
+
+			if (ai.show_tabs_flag) {
+				char *temp = changeTab(lineptr);
+				free(lineptr);
+				lineptr = temp;
+				n = strlen(lineptr) + 1;
+			}
+
 			if (ai.show_ends_flag) {
 				lineptr[read-1] = '\0';	// strip newline
 			}
+
+			if (ai.number_flag)
+				printf("%6lu\t", ++noLines);
 
 			printf("%s%s", lineptr, append);
 		}
@@ -57,3 +88,30 @@ main (int argc, char **argv)
 
 	exit(EXIT_SUCCESS);
 }
+
+char *
+changeTab(char *lineptr)
+{
+	int i;
+	int n = strlen(lineptr);
+
+	unsigned int tabCount = 0;
+	for(i = 0; i < n; ++i)
+		if (lineptr[i] == '\t')
+			++tabCount;
+
+	char *newString = malloc(n + tabCount + 1);	// 1 for '\0'
+
+	int j;
+	for(i = 0, j = 0; i < n; ++i)
+		if (lineptr[i] == '\t') {
+			newString[j++] = '^';
+			newString[j++] = 'I';
+		}
+		else {
+			newString[j++] = lineptr[i];
+		}
+
+	return newString;
+}
+			
